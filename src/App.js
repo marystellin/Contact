3397ddefb60baed38d5cfb79377c6215f6ae801b
marystellin <marystellinmca@gmail.com>
2019-11-05 23:11:06 +0530

    form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import React from 'react';
-import Axios from 'axios';
-import View from  './View' ;
-import { Jumbotron} from 'reactstrap';
-function App(){
-  const [input,setInput]=React.useState()
-  const [input1,setInput1]=React.useState()
-  const [input2,setInput2]=React.useState()
+import Axios from'axios';
+import View from  './View';
+import Form from './Form';
+const App=()=>{
   const [list,setlist]=React.useState([])
+  const[page,setpage]=React.useState("view")
   const getting=()=>{
     Axios.get("http://localhost:4000/value").then(res=>{
       setlist(res.data);
@@ -16,47 +14,127 @@ function App(){
   React.useEffect(()=>{
     getting()
   },[]);
-  
-  const getName=(event)=>
-  {
-    setInput(event.target.value)
-  }
-const getEmail=(event1)=>
-{
-  setInput1(event1.target.value)
-}
-const getPhone=(event2)=>
-{
-  setInput2(event2.target.value)
-}
-const getValue=(input,input1,input2)=>
+  const getValue=(input,input1,input2)=>
 {
   Axios.post("http://localhost:4000/add",{Name:input,Email:input1,Phone:input2})
   .then(success=>{
     setlist([...list,success.data])
    getting();
+   setpage("view")
 
-    setInput("");
-    setInput1("");
-    setInput2("");
+   
   })
 }
-return(<div>
-   <Jumbotron>
-  <h2>CONTACT MANAGER</h2>
-  {"ENTER YOUR NAME:"}<input type="text" onChange={getName} value={input}></input>
-  {"ENTER YOUR EMAIL:"}<input type="text" onChange={getEmail} value={input1}></input>
-  {"ENTER YOUR NUMBER:"}<input type="text" onChange={getPhone} value={input2}></input>
-{/* <button onClick={getValue}>SUBMIT</button> */}
-  <button onClick={()=>{
-    getValue(input,input1,input2);
-  }}>submit</button>
-  {/* <span>{input}{input1}{intput2}</span> */}
-  </Jumbotron>
-  <View List={list} setList={setlist} getting={getting} />
- </div>)
+  
+  return(
+    <div>
+      {
+      page==="view"?(<View List={list} setList={setlist} getting={getting} setpage={setpage} />):(<Form getValue={getValue}/>)
+      }
+       <button onClick={
+        ()=>{
+          setpage("form")
+      }}>AddContact</button>
+    </div>
+  )
 }
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import Axios from 'axios';
+// import View from  './View' ;
+// import { Jumbotron} from 'reactstrap';
+// function App(){
+//   const [input,setInput]=React.useState()
+//   const [input1,setInput1]=React.useState()
+//   const [input2,setInput2]=React.useState()
+//   const [list,setlist]=React.useState([])
+//   const[page,setpage]=React.useState("view")
+//   const getting=()=>{
+//     Axios.get("http://localhost:4000/value").then(res=>{
+//       setlist(res.data);
+//     });
+//   }
+//   React.useEffect(()=>{
+//     getting()
+//   },[]);
+  
+//   const getName=(event)=>
+//   {
+//     setInput(event.target.value)
+//   }
+// const getEmail=(event1)=>
+// {
+//   setInput1(event1.target.value)
+// }
+// const getPhone=(event2)=>
+// {
+//   setInput2(event2.target.value)
+// }
+// const getValue=(input,input1,input2)=>
+// {
+//   Axios.post("http://localhost:4000/add",{Name:input,Email:input1,Phone:input2})
+//   .then(success=>{
+//     setlist([...list,success.data])
+//    getting();
+
+//     setInput("");
+//     setInput1("");
+//     setInput2("");
+//   })
+// }
+// return(<div>
+//   {
+//     page==="view"?(<View List={list} setList={setlist} getting={getting} />):(<App/>)
+//   }
+//    <Jumbotron>
+//   <h2>CONTACT MANAGER</h2>
+//   {"ENTER YOUR NAME:"}<input type="text" onChange={getName} value={input}></input>
+//   {"ENTER YOUR EMAIL:"}<input type="text" onChange={getEmail} value={input1}></input>
+//   {"ENTER YOUR NUMBER:"}<input type="text" onChange={getPhone} value={input2}></input>
+// {/* <button onClick={getValue}>SUBMIT</button> */}
+//   <button onClick={()=>{
+//     setpage("app")
+//     getValue(input,input1,input2);
+//   }}>submit</button>
+//   {/* <span>{input}{input1}{intput2}</span> */}
+//   </Jumbotron>
+  
+//  </div>)
+// }
+// export default App;
 
 
 
